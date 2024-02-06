9969d26769b2b619c487dd6b6eb360d11bd228d7
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-02-06 17:04:10 -0300

    feat(poll): add route /polls to create a new poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
-import fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
+import fastify from 'fastify'
+import { z } from 'zod'
 
-const app = fastify();
+const app = fastify()
+const prisma = new PrismaClient()
 
-app.get("/", () => {
-  return "Hello World"
-});
+app.post('/polls', async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string()
+  })
 
-app.listen(({port: 3333})).then(() => console.log("Server is running on port 3333"))
+  const { title } = createPollBody.parse(request.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title
+    }
+  })
+
+  return await reply.status(201).send({ pollId: poll.id })
+})
+
+app.listen(({ port: 3333 })).then(() => { console.log('Server is running on port 3333') }).catch(error => { console.error(error) })
