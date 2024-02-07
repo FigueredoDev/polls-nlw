c5e3b029556b67803a40624dcb24a6f9f99721b7
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-02-07 09:55:20 -0300

    refactor(routes): extract routes from server.ts to a separate module

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,23 +1,8 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { prisma } from '../lib/prisma'
+import { createPoll } from '../routes/create-poll'
 
 const app = fastify()
 
-app.post('/polls', async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string()
-  })
-
-  const { title } = createPollBody.parse(request.body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title
-    }
-  })
-
-  return await reply.status(201).send({ pollId: poll.id })
-})
+void app.register(createPoll)
 
 app.listen(({ port: 3333 })).then(() => { console.log('Server is running on port 3333') }).catch(error => { console.error(error) })
---
+++ src/routes/create-poll.ts
@@ -0,0 +1,21 @@
+import { type FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+import z from 'zod'
+
+export async function createPoll (app: FastifyInstance): Promise<void> {
+  app.post('/polls', async (request, reply) => {
+    const createPollBody = z.object({
+      title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+      data: {
+        title
+      }
+    })
+
+    return await reply.status(201).send({ pollId: poll.id })
+  })
+}
