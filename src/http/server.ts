b91ad854bab13943d9730c1719e017e0bed89121
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-02-07 15:42:16 -0300

    feat(router): add route to fetch poll by id

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import { createPoll } from '../routes/create-poll'
+import { getPoll } from '../routes/get-poll'
 
 const app = fastify()
 
 void app.register(createPoll)
+void app.register(getPoll)
 
 app.listen(({ port: 3333 })).then(() => { console.log('Server is running on port 3333') }).catch(error => { console.error(error) })
