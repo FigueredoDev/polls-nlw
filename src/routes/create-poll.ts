44fab6eb61700823633da4c84722f09d33752e95
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-02-07 15:26:59 -0300

    feat(poll): add feature to send poll options when create a new poll

--- src/routes/create-poll.ts
+++ src/routes/create-poll.ts
@@ -5,14 +5,20 @@ import z from 'zod'
 export async function createPoll (app: FastifyInstance): Promise<void> {
   app.post('/polls', async (request, reply) => {
     const createPollBody = z.object({
-      title: z.string()
+      title: z.string(),
+      options: z.array(z.string())
     })
 
-    const { title } = createPollBody.parse(request.body)
+    const { title, options } = createPollBody.parse(request.body)
 
     const poll = await prisma.poll.create({
       data: {
-        title
+        title,
+        options: {
+          createMany: {
+            data: options.map(title => ({ title }))
+          }
+        }
       }
     })
 
