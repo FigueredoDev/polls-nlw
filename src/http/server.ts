ca0f35f12141c4d2df6d317704750b75db736e8f
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-02-08 16:44:34 -0300

    feat(routes): add route to vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,18 @@
 import fastify from 'fastify'
 import { createPoll } from '../routes/create-poll'
 import { getPoll } from '../routes/get-poll'
+import { voteOnPoll } from '../routes/vote-on-poll'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
+void app.register(cookie, {
+  secret: 'pools-app-nlw',
+  hook: 'onRequest'
+})
+
 void app.register(createPoll)
 void app.register(getPoll)
+void app.register(voteOnPoll)
 
 app.listen(({ port: 3333 })).then(() => { console.log('Server is running on port 3333') }).catch(error => { console.error(error) })
---
+++ src/routes/vote-on-poll.ts
@@ -0,0 +1,63 @@
+import { type FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+import z from 'zod'
+import { randomUUID } from 'crypto'
+
+export async function voteOnPoll (app: FastifyInstance): Promise<void> {
+  app.post('/polls/:pollId/votes', async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid()
+    })
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid()
+    })
+
+    const { pollOptionId } = voteOnPollBody.parse(request.body)
+    const { pollId } = voteOnPollParams.parse(request.params)
+
+    let { sessionId } = request.cookies
+
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId
+          }
+        }
+      })
+
+      if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id
+          }
+        })
+      } else if (userPreviousVoteOnPoll) {
+        return await reply.status(400).send({ message: 'You already voted on this poll' })
+      }
+    }
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      void reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true
+      })
+    }
+
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId
+      }
+    })
+
+    return await reply.status(201).send()
+  })
+}
