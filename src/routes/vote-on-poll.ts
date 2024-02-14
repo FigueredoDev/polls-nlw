1747db1f9991e66647a5ab387da77095ee036200
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-02-14 16:29:54 -0300

    feat(polls): add votes on redis

--- src/routes/vote-on-poll.ts
+++ src/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { type FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import z from 'zod'
 import { randomUUID } from 'crypto'
+import { redis } from '../lib/redis'
 
 export async function voteOnPoll (app: FastifyInstance): Promise<void> {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -34,6 +35,8 @@ export async function voteOnPoll (app: FastifyInstance): Promise<void> {
             id: userPreviousVoteOnPoll.id
           }
         })
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       } else if (userPreviousVoteOnPoll) {
         return await reply.status(400).send({ message: 'You already voted on this poll' })
       }
@@ -57,6 +60,8 @@ export async function voteOnPoll (app: FastifyInstance): Promise<void> {
         pollOptionId
       }
     })
+
+    await redis.zincrby(pollId, 1, pollOptionId)
 
     return await reply.status(201).send()
   })
